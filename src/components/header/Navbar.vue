26269860a36c20315a679137615cfc92a96e425f
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-04-16 07:43:49 +0100

    basic folder structure

---
+++ src/components/header/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav>
+    <ul>
+      <li><a href="">Home</a></li>
+      <li><a href="">Shop</a></li>
+      <li><a href="">About</a></li>
+      <li><a href="">Contact</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
+
+<style scoped>
+
+</style>
